40594adbbaaadcbf4a475f97cf284af8dd6149d8
harsh-suman5 <harshsuman15@gmail.com>
2025-12-01 22:17:30 +0530

    some codes are updated

---
+++ garbage_collection.py
@@ -0,0 +1,20 @@
+from pydoc import classname
+
+
+class garbage:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __del__(self):
+      class_name = self.__class__.__name__
+      print (class_name, "destroyed")
+# prints the ids of the obejcts
+
+pt1 = garbage()
+pt2 = pt1
+pt3 = pt1
+print (id(pt1), id(pt2), id(pt3))
+del pt1
+del pt2
+del pt3
+
---
+++ python- class attribute.py
@@ -0,0 +1,10 @@
+# the properties or variable define inside a class is called atributes.
+#accessing class attribute
+
+class employee:
+    name  = "harsh"
+    age  = 23
+emp = employee()
+print("name of the employee: ", emp.name)
+print("age of the employee: ", emp.age)
+
