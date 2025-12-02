ca7c3768d1f1873556e72362d2a55ad6354ca887
harsh-suman5 <harshsuman457@gmail.com>
2025-12-02 22:20:20 +0530

    accessing class attribute

---
+++ classatrribute.py
@@ -0,0 +1,8 @@
+# Accessing Class Attributes
+class employee:
+    name  = 'harsh'
+    salary = '40000' 
+emp = employee()
+print("name of the employee: ",emp.name)
+print("salary of the employee: ", emp.salary)
+
